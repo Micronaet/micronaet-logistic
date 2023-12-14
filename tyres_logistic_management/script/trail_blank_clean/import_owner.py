129939e7e60c5fa477c80702d3cfa89035d9542f
nicothebrush <nicola.riolini@gmail.com>
2023-12-14 10:34:11 +0100

    add script for trail blaks

---
+++ tyres_logistic_management/script/trail_blank_clean/import_owner.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# ODOO (ex OpenERP)
+# Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<http://www.micronaet.it>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+import os
+import pdb
+import sys
+import erppeek
+try:
+    import ConfigParser as configparser
+except:
+    import configparser
+
+
+# -----------------------------------------------------------------------------
+# Read configuration parameter:
+# -----------------------------------------------------------------------------
+cfg_file = os.path.expanduser('../odoo.cfg')
+
+config = configparser.ConfigParser()
+config.read([cfg_file])
+dbname = config.get('dbaccess', 'dbname')
+user = config.get('dbaccess', 'user')
+pwd = config.get('dbaccess', 'pwd')
+server = config.get('dbaccess', 'server')
+port = config.get('dbaccess', 'port')   # verify if it's necessary: getint
+
+# -----------------------------------------------------------------------------
+# Connect to ODOO:
+# -----------------------------------------------------------------------------
+print('Connect to ODOO')
+odoo = erppeek.Client(
+    'http://%s:%s' % (
+        server, port),
+    db=dbname,
+    user=user,
+    password=pwd,
+    )
+
+partner_pool = odoo.model('res.partner')
+partner_ids = partner_pool.search([
+    '|',
+    ('name', '=ilike', '% '),
+    ('name', '=ilike', ' %'),    
+    ])
+    
+if not partner_ids:
+    print('Not necessary')
+now = str(datetime.now()).replace('/', '_').replace('-', '').replace(':', '_')
+log_f = open('./log/%s.log' % now, 'w')    
+pdb.set_trace()
+for partner in partner_pool.browse(partner_ids):
+    name = partner.name or ''
+    new_name = name.strip()
+    partner_pool.write([partner.id], {
+        'name': new_name,
+        })
+    print('Update "%s" in "%s"' % (name, new_name))   
+
