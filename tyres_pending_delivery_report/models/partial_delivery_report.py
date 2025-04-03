6df33d5928db0d18dc47f2e5a19defc0a684283e
nicothebrush <nicola.riolini@gmail.com>
2025-04-03 14:07:55 +0200

    Fast commit

--- tyres_pending_delivery_report/models/partial_delivery_report.py
+++ tyres_pending_delivery_report/models/partial_delivery_report.py
@@ -136,7 +136,7 @@ class SaleOrder(models.AbstractModel):
                 #line.logistic_delivered_qty,
                 #line.logistic_undelivered_qty,
             ], default_format=f_white_text)
-            excel_pool.row_height(ws_name, [row], height=18 * load_total)
+            excel_pool.row_height(ws_name, [row], height=16 * load_total)
 
         # ---------------------------------------------------------------------
         # Save file:
