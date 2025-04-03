81ead6a8d38f6a21f850ea4be5363fe627c8cb92
nicothebrush <nicola.riolini@gmail.com>
2025-04-03 15:59:42 +0200

    Fast commit

--- tyres_pending_delivery_report/models/partial_delivery_report.py
+++ tyres_pending_delivery_report/models/partial_delivery_report.py
@@ -136,6 +136,7 @@ class SaleOrder(models.AbstractModel):
                 (int(line.logistic_remain_qty), f_white_number),
 
                 '\n'.join(load_comment),  # Supply detail (supplier, q., data)
+
                 # Q. block:
                 #line.logistic_covered_qty,
                 #line.logistic_uncovered_qty,
