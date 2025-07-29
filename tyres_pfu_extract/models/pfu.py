ccd3dfe313a7708cc6d920bfd5b0ba375ae70730
nicothebrush <nicola.riolini@gmail.com>
2025-07-29 16:47:06 +0200

    Fast commit

--- tyres_pfu_extract/models/pfu.py
+++ tyres_pfu_extract/models/pfu.py
@@ -366,6 +366,9 @@ class StockPickingPfuExtractWizard(models.TransientModel):
         }
         supplier_category_move = {}
         moves = move_pool.search(domain)
+        if not moves:
+            raise exceptions.Warning('Nessun movimento nel periodo -{} mesi'.format(period))
+
         pdb.set_trace()
         _logger.info('Found # {} sale Extra from Interal stock lines'.format(len(moves)))
         for move in moves:
