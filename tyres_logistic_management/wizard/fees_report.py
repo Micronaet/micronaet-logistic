57233f445f84a76a234bce7cf6374b187a4e5ee6
NicoTheBrush <nicola.riolini@gmail.com>
2026-07-07 13:01:42 +0200

    Fast commit

--- tyres_logistic_management/wizard/fees_report.py
+++ tyres_logistic_management/wizard/fees_report.py
@@ -27,6 +27,7 @@ import logging
 from odoo import api, fields, models, tools, exceptions, SUPERUSER_ID
 from odoo.addons import decimal_precision as dp
 from odoo.tools.translate import _
+import json
 
 _logger = logging.getLogger(__name__)
 
