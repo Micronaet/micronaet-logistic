ef78604ce37bcf197d3a54965462869412337499
NicoTheBrush <nicola.riolini@gmail.com>
2026-07-28 17:35:26 +0200

    Fast commit

--- tyres_logistic_management/wizard/fees_report.py
+++ tyres_logistic_management/wizard/fees_report.py
@@ -585,6 +585,7 @@ class LogisticFeesHeaderInerit(models.Model):
                 vat_excluded_rate,
                 triangle_invoice,
                 ))
+            _logger.info('{} {}'.format(partner.name, vat_excluded_rate))
             pdb.set_trace()
 
         # Return collected NC list:
