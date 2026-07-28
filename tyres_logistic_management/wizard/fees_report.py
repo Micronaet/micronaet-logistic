8c51ec0faa7358d240c999ba0fdad2135005899f
NicoTheBrush <nicola.riolini@gmail.com>
2026-07-28 17:06:32 +0200

    Fast commit

--- tyres_logistic_management/wizard/fees_report.py
+++ tyres_logistic_management/wizard/fees_report.py
@@ -947,8 +947,10 @@ class LogisticFeesExtractWizard(models.TransientModel):
                 if customer_mode == 'b2c':
                     page = 'B2C'
                 elif customer_mode == 'b2b':
-                    import pdb; pdb.set_trace()
-                    total += total * vat / 100.0  # In B2B integrate VAT
+                    try:
+                        total += total * vat / 100.0  # In B2B integrate VAT
+                    except:
+                        import pdb; pdb.set_trace()
 
                     if payment_code == '026':
                         page = 'B2B SEPA'
